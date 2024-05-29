7b12df00bb15b41510edeefc62786ba92e7575d1
julialuyten <153140302+julialuyten@users.noreply.github.com>
2024-05-28 17:09:06 -0700

    Create Humidity.js

---
+++ VCApp/Humidity.js
@@ -0,0 +1,27 @@
+    // Humidity.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Humidity = () => {
+  return (
+    <View>
+      <Text>Humidity 
+        Associated illnesses:
+        See temperature-related risks above 
+        Increased growth of mold and fungus
+        Can worsen air quality, see lower air quality-related indicators
+        Chronic kidney disease associated with increased sweating
+        Safe range: Indoor humidity between 30 and 50% is ideal for human health.
+        Ways to reduce harm:
+        Dehumidify 
+        See reducing high temperature harms </Text>
+    </View>
+  );
+};
+
+export default Humidity;
+
+
+
+
+
