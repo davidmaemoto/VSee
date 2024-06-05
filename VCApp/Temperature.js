9b981db00614e0b6f44a25afa317c92405cf9a3b
julialuyten <153140302+julialuyten@users.noreply.github.com>
2024-06-04 17:04:53 -0700

    Update Temperature.js

--- VCApp/Temperature.js
+++ VCApp/Temperature.js
@@ -1,26 +1,57 @@
-// Temperature.js
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 
 const Temperature = () => {
   return (
-    <View>
-      <Text>Temperature
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.heading}>Temperature</Text>
+      <Text style={styles.text}>
         Associated illnesses:
-        Heat cramps
-        Heat exhaustion
-        Heatstroke
-        Hyperthermia 
-        Worsened chronic conditions, particularly pulmonary and cardiovascular-related conditions.
-        Safe range: In the range of 90-105 degrees F, risk of heat exhaustion and heat cramps begins. With strenuous work, this risk begins at 77 degrees F 
+        {"\n\n"}
+        - Heat cramps
+        {"\n"}
+        - Heat exhaustion
+        {"\n"}
+        - Heatstroke
+        {"\n"}
+        - Hyperthermia
+        {"\n"}
+        - Worsened chronic conditions, particularly pulmonary and cardiovascular-related conditions.
+        {"\n\n"}
+        Safe range: In the range of 90-105 degrees F, the risk of heat exhaustion and heat cramps begins. With strenuous work, this risk begins at 77 degrees F.
+        {"\n\n"}
         Ways to reduce harm:
-        Apply sunscreen 
-        Wear loose, lightweight clothing
-        Take breaks if possible 
-        Drink fresh water
-        Seek shade 
-</Text>
-    </View>
+        {"\n"}
+        - Apply sunscreen
+        {"\n"}
+        - Wear loose, lightweight clothing
+        {"\n"}
+        - Take breaks if possible
+        {"\n"}
+        - Drink fresh water
+        {"\n"}
+        - Seek shade
+      </Text>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  heading: {
+    fontSize: 24,
+    marginBottom: 20,
+    fontFamily: 'Arial', // Match font family
+    color: '#450', // Match text color
+  },
+  text: {
+    fontSize: 16,
+    fontFamily: 'Arial', // Match font family
+    color: '#450', // Match text color
+  },
+});
+
 export default Temperature;
